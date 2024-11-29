9d0db35b6f317a6ac011e3f6d4d8f337bd27b8be
Isaac Ch <89037579+Txaverria@users.noreply.github.com>
2024-11-29 17:53:31 -0600

    Update ofertasLaborales.js

--- api/models/ofertasLaborales.js
+++ api/models/ofertasLaborales.js
@@ -2,22 +2,6 @@ const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({}, { strict: false });
 
-  // titulo: { type: String },
-  // descripcion: { type: String },
-  // salario: { type: Number },
-  // ubicacion: { type: String },
-  // tipo: {
-  //   type: String,
-  //   enum: ["tiempo completo", "medio tiempo"],
-  // },
-  // modalidad: {
-  //   type: String,
-  //   enum: ["híbrido", "presencial"],
-  // },
-  // fechaPublicacion: { type: Date, default: Date.now },
-  // fechaExpiracion: { type: Date },
-  // solicitantes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Usuario" }],
-
 const OfertaLaboral = mongoose.model("OfertaLaboral", schema, "ofertasLaborales");
 
 module.exports = OfertaLaboral;
